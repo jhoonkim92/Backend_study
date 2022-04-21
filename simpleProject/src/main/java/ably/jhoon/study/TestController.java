24b55b0034c084d80fc4664fa3b871662dfce5f1
jhoonkim92 <jhoonkim92@gmail.com>
2022-04-21 17:24:50 +0900

    first commit

---
+++ simpleProject/src/main/java/ably/jhoon/study/TestController.java
@@ -0,0 +1,12 @@
+package ably.jhoon.study;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/")
+	  public String index() {
+	    return "demo project!!";
+	  }
+}
